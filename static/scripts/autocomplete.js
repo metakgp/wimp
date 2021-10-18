67f34e2742fe0f5b541cc7bcf95d1e429d56e1e9
wweverma1 <wweverma1@gmail.com>
2021-10-19 01:16:21 +0530

    scrolling the professors list using arrow keys

--- static/scripts/autocomplete.js
+++ static/scripts/autocomplete.js
@@ -34,7 +34,7 @@ function autocomplete(datalist, worker) {
     function displayResult(results) {    
         var content = '';
         for (r of results) {
-            content += '<a href="/?prof=' + datalist[r.index] + '" class="list-group-item list-group-item-action" class="suggestion-item">' + datalist[r.index] + '</a>'
+            content += '<li><a href="/?prof=' + datalist[r.index] + '" tabindex="1" class="list-group-item list-group-item-action" class="suggestion-item">' + datalist[r.index] + '</a></li>'
         }
         
         suggestions.querySelector('ul').innerHTML = content;
@@ -42,6 +42,22 @@ function autocomplete(datalist, worker) {
         suggestions.querySelectorAll('a').forEach(function(elem) {
             elem.addEventListener('mousedown', function(e) {prof_query.value = e.target.innerText;});
         });
+        
+        var list = document.getElementById('list'); // targets the <ul>
+        var first = list.firstChild; // targets the first <li>
+        var maininput = document.getElementById('prof_query');  // targets the input, which triggers the functions populating the list
+        document.onkeydown = function(e) { // listen to keyboard events
+            switch (e.keyCode) {
+            case 38: // if the UP key is pressed
+                if (document.activeElement == (maininput || first)) { break; } // stop the script if the focus is on the input or first element
+                else { document.activeElement.parentNode.previousSibling.firstChild.focus(); } // select the element before the current, and focus it
+                break;
+            case 40: // if the DOWN key is pressed
+                if (document.activeElement == maininput) { first.firstChild.focus(); } // if the currently focused element is the main input --> focus the first <li>
+                else { document.activeElement.parentNode.nextSibling.firstChild.focus(); } // target the currently focused element -> <a>, go up a node -> <li>, select the next node, go down a node and focus it
+            break;
+            }
+        }
         
     }
 
