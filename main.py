73fc8db613bd1ad2ed9984789b049d1acf6b972a
rajivharlalka <rajivharlalka009@gmail.com>
2023-03-05 20:45:01 +0530

    Formatted Scripts

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3.6
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
 from classes import *
 
 import werkzeug
+
 werkzeug.cached_property = werkzeug.utils.cached_property
 from robobrowser import RoboBrowser
 
@@ -18,25 +19,26 @@ import sys
 path = os.path.abspath(os.path.dirname(__file__))
 
 try:
-
-    with open(os.path.join(path, 'data/data.json'), 'r') as f:
+    with open(os.path.join(path, "data/data.json"), "r") as f:
         profs_dict = CaseInsensitiveDict(json.load(f))
 
 except FileNotFoundError:
     profs_dict = CaseInsensitiveDict({})
 
-DEPT_KEY = 'dept'
-WEBSITE_KEY = 'website'
-TIMETABLE_KEY = 'timetable'
-KGP_WEBSITE_URL = 'http://www.iitkgp.ac.in/'
-DEPT_FETCH_URL = 'http://www.iitkgp.ac.in/facultylist?processOn=onload&colName=&searchContent=&_=1538283022101'
-TIMETABLE_FETCH_URL = 'https://erp.iitkgp.ac.in/Acad/timetable_track.jsp?action=second&dept=%s'
+DEPT_KEY = "dept"
+WEBSITE_KEY = "website"
+TIMETABLE_KEY = "timetable"
+KGP_WEBSITE_URL = "http://www.iitkgp.ac.in/"
+DEPT_FETCH_URL = "http://www.iitkgp.ac.in/facultylist?processOn=onload&colName=&searchContent=&_=1538283022101"
+TIMETABLE_FETCH_URL = (
+    "https://erp.iitkgp.ac.in/Acad/timetable_track.jsp?action=second&dept=%s"
+)
 
 
 def get_time(slot):
     # Obtains time for each slot from 'data/slots.1' file
 
-    with open(os.path.join(path, 'data/slots.1')) as f:
+    with open(os.path.join(path, "data/slots.1")) as f:
         for line in f:
             if line.startswith(slot):
                 return line.split()[1:]
@@ -45,23 +47,25 @@ def get_time(slot):
 def parse_html(dep):
     # Get prof timetable
     try:
-
         br.open(TIMETABLE_FETCH_URL % dep)
         # print(br.response.content)
         html = br.response.content
-        soup = BeautifulSoup(html, 'lxml')
-        html = soup.find_all('table')[4]
+        soup = BeautifulSoup(html, "lxml")
+        html = soup.find_all("table")[4]
         print("Fetched for %s" % dep)
     except Exception as err:
         print("Can't fetch %s" % dep)
         print(err)
         return
 
-    table_data = [[cell.text for cell in row("td")] for row in BeautifulSoup(str(html), 'lxml')("tr")]
+    table_data = [
+        [cell.text for cell in row("td")]
+        for row in BeautifulSoup(str(html), "lxml")("tr")
+    ]
     table_data = [row for row in table_data[2:] if len(row) == 7]
 
     # Test code for writing data obtained to, uncomment when want to test
-    with open('data/table_test', 'w') as f:
+    with open("data/table_test", "w") as f:
         f.write(str(table_data))
 
     # Get prof department
@@ -75,30 +79,29 @@ def parse_html(dep):
 
     """
     dept_resp = requests.get(DEPT_FETCH_URL)
-    dept_raw_data = json.loads(dept_resp.content)['data']
+    dept_raw_data = json.loads(dept_resp.content)["data"]
     dept_data = CaseInsensitiveDict({})
 
     for prof in dept_raw_data:
-        name = re.findall(r'>(.+?)<', prof['faculty'])[0]
-        name = name.replace('  ', '')
-        dept = prof['dept_code']
+        name = re.findall(r">(.+?)<", prof["faculty"])[0]
+        name = name.replace("  ", "")
+        dept = prof["dept_code"]
 
-        soup = BeautifulSoup(prof['faculty'], 'lxml')
+        soup = BeautifulSoup(prof["faculty"], "lxml")
 
-        for tag in soup.findAll('a', href=True):
-            href = tag['href']
+        for tag in soup.findAll("a", href=True):
+            href = tag["href"]
 
             website = KGP_WEBSITE_URL + href
 
-            dept_data[name] = {'dept': dept,
-                               'website': website}
-            with open('data/dept_data', 'w') as f:
+            dept_data[name] = {"dept": dept, "website": website}
+            with open("data/dept_data", "w") as f:
                 json.dump(dept_data, f)
 
     for row in table_data:
-        prof_names = [name.title() for name in row[2].split(',')]
-        slots = [slot.replace(' ', '') for slot in row[5].split(',')]
-        venues = [venue.replace('Deptt.', 'Dept') for venue in row[6].split(',')]
+        prof_names = [name.title() for name in row[2].split(",")]
+        slots = [slot.replace(" ", "") for slot in row[5].split(",")]
+        venues = [venue.replace("Deptt.", "Dept") for venue in row[6].split(",")]
 
         for prof_name in prof_names:
             # print(prof_name)
@@ -107,14 +110,14 @@ def parse_html(dep):
                     profs_dict[prof_name] = {}
 
                     try:
-
                         profs_dict[prof_name][DEPT_KEY] = dept_data[prof_name][DEPT_KEY]
-                        profs_dict[prof_name][WEBSITE_KEY] = dept_data[prof_name][WEBSITE_KEY]
+                        profs_dict[prof_name][WEBSITE_KEY] = dept_data[prof_name][
+                            WEBSITE_KEY
+                        ]
 
                     except KeyError:
-
                         profs_dict[prof_name][DEPT_KEY] = dep
-                        profs_dict[prof_name][WEBSITE_KEY] = '#'
+                        profs_dict[prof_name][WEBSITE_KEY] = "#"
 
                     profs_dict[prof_name][TIMETABLE_KEY] = []
 
@@ -126,7 +129,7 @@ def parse_html(dep):
         return profs_dict
 
     else:
-        print('No records found for %s' % dep)
+        print("No records found for %s" % dep)
 
 
 def get_times(prof_name):
@@ -134,7 +137,6 @@ def get_times(prof_name):
     result = []
 
     try:
-
         result = data[prof_name][TIMETABLE_KEY]
 
         if result:
@@ -142,7 +144,6 @@ def get_times(prof_name):
             result = list(result for result, _ in itertools.groupby(result))
 
     except:
-
         pass
 
     return result
@@ -164,11 +165,9 @@ def get_attr(prof_name, key):
     result = ""
 
     try:
-
         result = data[prof_name][key]
 
     except:
-
         pass
 
     return result
@@ -179,7 +178,7 @@ def get_table(details):
 
     for i in range(5):
         for j in range(9):
-            tb.update({'%d%d' % (i, j): []})
+            tb.update({"%d%d" % (i, j): []})
 
     for times, venues in details:
         venues = set(v.strip() for v in venues)
@@ -191,26 +190,25 @@ def get_table(details):
 
 
 def populate_data(specific_dep=None):
-    if not os.getenv('JSESSIONID'):
+    if not os.getenv("JSESSIONID"):
         print("ERROR: Please set environment variable JSESSIONID!")
         sys.exit(1)
 
-    cookie = {
-        "JSESSIONID": os.getenv('JSESSIONID')
-    }
+    cookie = {"JSESSIONID": os.getenv("JSESSIONID")}
 
     # Browser
     global br
-    br = RoboBrowser(history=True,
-                     user_agent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0',
-                     parser='lxml'
-                     )
+    br = RoboBrowser(
+        history=True,
+        user_agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0",
+        parser="lxml",
+    )
 
     # Update cookie
     br.session.cookies.update(cookie)
 
-    with open(os.path.join(path, 'data/deps.4')) as f:
-        deps = f.read().split('\n')
+    with open(os.path.join(path, "data/deps.4")) as f:
+        deps = f.read().split("\n")
     # parse_html('EC')
     if specific_dep is None:
         for dep in deps:
@@ -218,19 +216,23 @@ def populate_data(specific_dep=None):
     else:
         parse_html(specific_dep)
 
-    with open(os.path.join(path, 'data/data.json'), 'w') as f:
+    with open(os.path.join(path, "data/data.json"), "w") as f:
         json.dump(profs_dict, f)
 
 
 def main():
     dep = None
-    dep = str(input('''Is there a specific dep which you want to enter (write dep code),leaving this will update for all deps:\n'''))
+    dep = str(
+        input(
+            """Is there a specific dep which you want to enter (write dep code),leaving this will update for all deps:\n"""
+        )
+    )
     if dep == "":
         dep = None
     populate_data(dep)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Run main to populate data
     main()
 
