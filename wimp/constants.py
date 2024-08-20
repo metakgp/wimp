76068fc4e08fe3220fded69fd969ac2a27eb804c
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-08-20 23:32:36 +0530

    feat: constants in a separate file

---
+++ wimp/constants.py
@@ -0,0 +1,8 @@
+KGP_WEBSITE_URL = "http://www.iitkgp.ac.in/"
+DEPT_FETCH_URL = "https://www.iitkgp.ac.in/Departments/fetchAllFacListByDept"
+TIMETABLE_FETCH_URL = "https://erp.iitkgp.ac.in/Acad/timetable_track.jsp?action=second&dept=%s"
+DEFAULT_REQUEST_HEADERS = {
+	'timeout': '20',
+	'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'
+}
+DEPARTMENT_CODES = ["AE", "AF", "AG", "AI", "AR", "AT", "BE", "BM", "BS", "BT", "CD", "CE", "CH", "CL", "CR", "CS", "CY", "DE", "DH", "EC", "EE", "EF", "ES", "ET", "FH", "GG", "GS", "HS", "ID", "IM", "IP", "IT", "KS", "MA", "ME", "MI", "MM", "MS", "MT", "NA", "NT", "PH", "RD", "RE", "RJ", "RO", "RT", "RW", "RX", "SD", "SE", "SH", "SL", "TS", "WM"]
